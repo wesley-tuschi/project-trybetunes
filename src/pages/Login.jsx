73bab8f27f775c9bbe442a8e597bea66e3c5a8c0
Wesley Tuschi <levituschi@gmail.com>
2023-03-13 17:55:35 -0300

    exercicio 2 completo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 
 class Login extends Component {
@@ -8,39 +10,63 @@ class Login extends Component {
     this.state = {
       name: '',
       isButtonDisabled: true,
+      isLoading: false,
     };
   }
 
-  handleInputChange = (event) => {
+  handleInputChange = ({ target }) => {
     const CRCT = 2;
-    const name = event.target.value;
+    const name = target.value;
     const isButtonDisabled = name.length <= CRCT;
     this.setState({ name, isButtonDisabled });
   };
 
+  handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    this.setState({ isLoading: false });
+
+    const { history } = this.props;
+    history.push('/search');
+  };
+
   render() {
-    const { name, isButtonDisabled } = this.state;
+    const { name, isButtonDisabled, isLoading } = this.state;
+    const loadingMessage = 'Carregando...';
+
     return (
       <div data-testid="page-login">
-        <form>
-          <label>
-            <input
-              data-testid="login-name-input"
-              type="text"
-              value={ name }
-              onChange={ this.handleInputChange }
-            />
-            <button
-              data-testid="login-submit-button"
-              disabled={ isButtonDisabled }
-            >
-              Entrar
-            </button>
-          </label>
-        </form>
+        { isLoading && <div>{ loadingMessage }</div> }
+        { !isLoading && (
+          <form>
+            <label>
+              <input
+                data-testid="login-name-input"
+                type="text"
+                value={ name }
+                onChange={ this.handleInputChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                disabled={ isButtonDisabled }
+                onClick={ this.handleLoginSubmit }
+              >
+                Entrar
+              </button>
+            </label>
+          </form>
+        )}
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(Login);
