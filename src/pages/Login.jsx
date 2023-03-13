8bc01aac91694a6a82e44afc31cfc80ddedf7299
Wesley Tuschi <levituschi@gmail.com>
2023-03-13 15:59:53 -0300

    Exercicio 2 50%

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  handleInputChange = (event) => {
+    const CRCT = 2;
+    const name = event.target.value;
+    const isButtonDisabled = name.length <= CRCT;
+    this.setState({ name, isButtonDisabled });
+  };
+
   render() {
+    const { name, isButtonDisabled } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label>
+            <input
+              data-testid="login-name-input"
+              type="text"
+              value={ name }
+              onChange={ this.handleInputChange }
+            />
+            <button
+              data-testid="login-submit-button"
+              disabled={ isButtonDisabled }
+            >
+              Entrar
+            </button>
+          </label>
+        </form>
+      </div>
     );
   }
 }
