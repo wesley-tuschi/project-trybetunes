1f0826a20aeeed2f8357a4f5afd2fb9f7e30c805
Wesley Tuschi <levituschi@gmail.com>
2023-03-19 23:00:36 -0300

    exercicio 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    artistName: '',
+    albumName: '',
+    songs: [],
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const albumId = match.params.id;
+    const results = await getMusics(albumId);
+    const songs = results.slice(1);
+
+    this.setState({
+      artistName: results[0].artistName,
+      albumName: results[0].collectionName,
+      songs,
+    });
+  }
+
   render() {
+    const { artistName, albumName, songs } = this.state;
+
     return (
       <div data-testid="page-album">
         <Header />
+        <div data-testid="artist-name">{artistName}</div>
+        <div data-testid="album-name">{albumName}</div>
+        <div data-testid="song-list">
+          {songs.map((song) => (
+            <MusicCard
+              key={ song.trackId }
+              trackName={ song.trackName }
+              previewUrl={ song.previewUrl }
+            />
+          ))}
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
